72cfc757144fc23c2ccc3d3c7508228f38c3f437
200431090 <66093697+200431090@users.noreply.github.com>
2020-06-18 01:32:49 -0400

    update

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -6,13 +6,14 @@
 # Task 2: Change the output to only show:
 #    the sum of the 3 numbers with a label
 #    the product of the 3 numbers with a label
-
-read -p "Input a No.1? " firstnum
-read -p "Input a No.2? " secondnum
-read -p "Input a No.3? " thirdnum
+# Ask for three numbers from the user by using the read command
+read -p "Input 1 ? " firstnum 
+read -p "Input 2 ? " secondnum
+read -p "Input 3 ? " thirdnum
 sum=$((firstnum + secondnum + thirdnum))
+# Sum of the three numbers of user is added into a variable
 product=$((firstnum * secondnum * thirdnum))
-
+# Product of the three numbers given by user is added into a variable
 cat <<EOF
 The sum of $firstnum , $secondnum and $thirdnum numbers are $sum .
 The product of $firstnum , $secondnum and $thirdnum numbers are $product .
--- bash/improveddice.sh
+++ bash/improveddice.sh
@@ -17,12 +17,13 @@
 # Tell the user we have started processing
 echo "We have started processing..."
 # roll the dice and save the results
-range=6
 bias=1
+range=6
+# Using the variable of the range or bias the dice is roll
 dice1=$(( RANDOM % range + bias ))
 dice2=$(( RANDOM % range + bias ))
-sum=$(( dice1 + dice2 ))
-average=$(( sum / 2 ))
-# display the results
+sum=$(( dice1 + dice2 )) # in this the total sum of the dice is outputted 
+average=$(( sum / 2 )) # In this we get the average of the dice
+# Shows the total summary of the dice which was rolled and it’s outcome
 echo "Rolled two times $dice1, $dice2 and a sum is $sum"
 echo "Rolled two times $dice1, $dice2 and an average is $average"
